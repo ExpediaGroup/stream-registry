ddc422aa55b0a95c4b99e62d980e6869a471f073
Rene X. Parra <552515+neoword@users.noreply.github.com>
2018-12-12 15:30:23 -0600

    Updating license header for DummyForJavaDoc

--- assembly/src/main/java/com/homeaway/streamingplatform/streamregistry/shaded/DummyForJavaDoc.java
+++ assembly/src/main/java/com/homeaway/streamingplatform/streamregistry/shaded/DummyForJavaDoc.java
@@ -1,4 +1,6 @@
-/*
+/* Copyright (c) 2018 Expedia Group.
+ * All rights reserved.  http://www.homeaway.com
+
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
