7af1466bc746a85a71f04cec19eea36731734a05
Prabhakaran Thatchinamoorthy <pthatchinamoorthy@homeaway.com>
2019-03-01 11:07:44 -0600

    Made the MDC Filters passed at runtime

--- core/src/main/java/com/homeaway/streamplatform/streamregistry/StreamRegistryApplication.java
+++ core/src/main/java/com/homeaway/streamplatform/streamregistry/StreamRegistryApplication.java
@@ -164,27 +164,24 @@ public class StreamRegistryApplication extends Application<StreamRegistryConfigu
 
         registerManagedContainer(environment, managedKStreams, managedInfraManager, managedKafkaProducer);
 
-        // Step 7 - Add MDCFilter for Diagnostics.
-        environment.jersey().register(new MDCFilter());
+        // Step 7 - Add MDCRequestFilter for Diagnostics.
+        registerFiltersForMDC(environment, configuration);
 
         // TODO: Make project completely based on unit tests (integration should be a separate project) (#100)
     }
 
-    private void registerFilters(Environment environment, StreamRegistryConfiguration configuration) {
+    private void registerFiltersForMDC(Environment environment, StreamRegistryConfiguration configuration) {
         try {
-            if (configuration.getRequestFilterClassNames() != null) {
-                for (String requestFilterClassName : configuration.getRequestFilterClassNames()) {
-                    environment.jersey().register(Utils.newInstance(requestFilterClassName, ContainerRequestFilter.class));
-                }
+            if (configuration.getRequestMDCFilterClassName() != null) {
+                ContainerRequestFilter mdcRequestFilter = Utils.newInstance(configuration.getRequestMDCFilterClassName(), ContainerRequestFilter.class);
+                environment.jersey().register(mdcRequestFilter);
             }
-
-            if (configuration.getResponseFilterClassNames() != null) {
-                for (String responseFilterClassName : configuration.getResponseFilterClassNames()) {
-                    environment.jersey().register(Utils.newInstance(responseFilterClassName, ContainerResponseFilter.class));
-                }
+            if (configuration.getResponseMDCFilterClassName() != null) {
+                ContainerResponseFilter mdcResponseFilter = Utils.newInstance(configuration.getResponseMDCFilterClassName(), ContainerResponseFilter.class);
+                environment.jersey().register(mdcResponseFilter);
             }
-        } catch (Exception e) {
-            throw new IllegalStateException(String.format("Error loading Jersey Filter from configuration %s",configuration),  e);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException(String.format("Error loading MDCFilter from configuration %s",configuration),  e);
         }
     }
 
