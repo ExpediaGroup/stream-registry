e2be9231ec22b4a420ea5807f3dc041cd08be2d9
Prabhakaran Thatchinamoorthy <pthatchinamoorthy@homeaway.com>
2019-03-01 18:10:58 -0600

    Added Java Doc

--- core/src/main/java/com/homeaway/streamplatform/streamregistry/UUIDMDCRequestFilter.java
+++ core/src/main/java/com/homeaway/streamplatform/streamregistry/UUIDMDCRequestFilter.java
@@ -22,9 +22,16 @@ import javax.ws.rs.container.ContainerRequestFilter;
 
 import org.slf4j.MDC;
 
+/**
+ * UUIDMDCRequestFilter class whose filter method would be called for each HttpRequest.
+ */
 public class UUIDMDCRequestFilter implements ContainerRequestFilter {
     public static final String MDC_REQUEST_UUID ="request-UUID";
 
+    /**
+     * Adds an key-value in MDC for each http-request which can be used for log and request tracking
+     * @param requestContext
+     */
     @Override
     public void filter(ContainerRequestContext requestContext) {
         MDC.put(MDC_REQUEST_UUID, UUID.randomUUID().toString());
