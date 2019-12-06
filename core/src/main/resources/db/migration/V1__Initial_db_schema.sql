739709cc9471a3efe588728664cd8d9a85bafed4
Andrzej Lobodzki <andrzej.lobodzki@protonmail.com>
2019-12-06 11:58:25 +0000

    Addressing PR comments.

--- core/src/main/resources/db/migration/V1__Initial_db_schema.sql
+++ core/src/main/resources/db/migration/V1__Initial_db_schema.sql
@@ -183,9 +183,9 @@ CREATE TABLE consumer_binding_tags (
 	consumer_binding_stream_name varchar(100) NOT NULL,
 	consumer_binding_stream_version int4 NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_pqwiavnk8mg354eqn50tkxvhv UNIQUE (tags_id),
-	CONSTRAINT fkhs0j1uycm0hfnha9d8y59o4e7 FOREIGN KEY (tags_id) REFERENCES tag(id),
-	CONSTRAINT fkjbmehtu8co8o1gwjqwrb2kkgp FOREIGN KEY (consumer_binding_consumer_name, consumer_binding_infrastructure_name, consumer_binding_infrastructure_zone, consumer_binding_stream_domain, consumer_binding_stream_name, consumer_binding_stream_version) REFERENCES consumer_binding(consumer_name, infrastructure_name, infrastructure_zone, stream_domain, stream_name, stream_version)
+	CONSTRAINT cbd_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT cbd_t_fk FOREIGN KEY (tags_id) REFERENCES tag(id),
+	CONSTRAINT cbd_cb_fk FOREIGN KEY (consumer_binding_consumer_name, consumer_binding_infrastructure_name, consumer_binding_infrastructure_zone, consumer_binding_stream_domain, consumer_binding_stream_name, consumer_binding_stream_version) REFERENCES consumer_binding(consumer_name, infrastructure_name, infrastructure_zone, stream_domain, stream_name, stream_version)
 );
 
 -- Drop table
@@ -199,9 +199,9 @@ CREATE TABLE consumer_tags (
 	consumer_stream_version int4 NOT NULL,
 	consumer_zone varchar(100) NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_rk2fr5r7ocegsq46vklswkvu1 UNIQUE (tags_id),
-	CONSTRAINT fkqbfa6brkql2cwnwk056cu2tnp FOREIGN KEY (consumer_name, consumer_stream_domain, consumer_stream_name, consumer_stream_version, consumer_zone) REFERENCES consumer(name, stream_domain, stream_name, stream_version, zone),
-	CONSTRAINT fks9w0keceqchfvhdg9f9j9a70i FOREIGN KEY (tags_id) REFERENCES tag(id)
+	CONSTRAINT ct_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT ct_c_fkey FOREIGN KEY (consumer_name, consumer_stream_domain, consumer_stream_name, consumer_stream_version, consumer_zone) REFERENCES consumer(name, stream_domain, stream_name, stream_version, zone),
+	CONSTRAINT ct_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id)
 );
 
 -- Drop table
@@ -211,9 +211,9 @@ CREATE TABLE consumer_tags (
 CREATE TABLE domain_tags (
 	domain_name varchar(100) NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_boeunkl6y9lk0fqke7biwvlmq UNIQUE (tags_id),
-	CONSTRAINT fkcw2kgmuhk3ypn8l98e4lnagvf FOREIGN KEY (domain_name) REFERENCES domain(name),
-	CONSTRAINT fkkhpu6dsy48ryt2weaoikonpp0 FOREIGN KEY (tags_id) REFERENCES tag(id)
+	CONSTRAINT dt_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT dt_d_fkey FOREIGN KEY (domain_name) REFERENCES domain(name),
+	CONSTRAINT dt_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id)
 );
 
 -- Drop table
@@ -224,9 +224,9 @@ CREATE TABLE infrastructure_tags (
 	infrastructure_name varchar(100) NOT NULL,
 	infrastructure_zone varchar(100) NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_c2d90c38vee279gijvuo3drh8 UNIQUE (tags_id),
-	CONSTRAINT fknfcbulj4n4u6y7woa9e2ysmr3 FOREIGN KEY (infrastructure_name, infrastructure_zone) REFERENCES infrastructure(name, zone),
-	CONSTRAINT fktagg7kpdv9a4e5gmu97q1vn60 FOREIGN KEY (tags_id) REFERENCES tag(id)
+	CONSTRAINT it_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT it_i_fkey FOREIGN KEY (infrastructure_name, infrastructure_zone) REFERENCES infrastructure(name, zone),
+	CONSTRAINT it_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id)
 );
 
 -- Drop table
@@ -241,9 +241,9 @@ CREATE TABLE producer_binding_tags (
 	producer_binding_stream_name varchar(100) NOT NULL,
 	producer_binding_stream_version int4 NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_qnlwwsaea10ns6u3228e0w74c UNIQUE (tags_id),
-	CONSTRAINT fkg7a3lawsso2ngxubfkxh08nf5 FOREIGN KEY (tags_id) REFERENCES tag(id),
-	CONSTRAINT fkpewya0dt9slqoano7e5ivv444 FOREIGN KEY (producer_binding_infrastructure_name, producer_binding_infrastructure_zone, producer_binding_producer_name, producer_binding_stream_domain, producer_binding_stream_name, producer_binding_stream_version) REFERENCES producer_binding(infrastructure_name, infrastructure_zone, producer_name, stream_domain, stream_name, stream_version)
+	CONSTRAINT pbt_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT pbt_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id),
+	CONSTRAINT pbt_pb_fkey FOREIGN KEY (producer_binding_infrastructure_name, producer_binding_infrastructure_zone, producer_binding_producer_name, producer_binding_stream_domain, producer_binding_stream_name, producer_binding_stream_version) REFERENCES producer_binding(infrastructure_name, infrastructure_zone, producer_name, stream_domain, stream_name, stream_version)
 );
 
 -- Drop table
@@ -257,9 +257,9 @@ CREATE TABLE producer_tags (
 	producer_stream_version int4 NOT NULL,
 	producer_zone varchar(100) NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_md22ps9pqyvihngfbiq43bvqr UNIQUE (tags_id),
-	CONSTRAINT fk8omxlah620qoe6k3fl5lifeew FOREIGN KEY (producer_name, producer_stream_domain, producer_stream_name, producer_stream_version, producer_zone) REFERENCES producer(name, stream_domain, stream_name, stream_version, zone),
-	CONSTRAINT fkjdro73bjhxlv9pl2r52ru9yx5 FOREIGN KEY (tags_id) REFERENCES tag(id)
+	CONSTRAINT pt_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT pt_p_fkey FOREIGN KEY (producer_name, producer_stream_domain, producer_stream_name, producer_stream_version, producer_zone) REFERENCES producer(name, stream_domain, stream_name, stream_version, zone),
+	CONSTRAINT pt_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id)
 );
 
 -- Drop table
@@ -270,9 +270,9 @@ CREATE TABLE rword_schema_tags (
 	rword_schema_skdomain varchar(100) NOT NULL,
 	rword_schema_skname varchar(100) NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_9m26e4v7cveajbtoyqbbj1r8a UNIQUE (tags_id),
-	CONSTRAINT fkiqc1p5grut0w72ut48pmkp4mt FOREIGN KEY (tags_id) REFERENCES tag(id),
-	CONSTRAINT fkojeciao6pur9ej3p48m4ntpdq FOREIGN KEY (rword_schema_skdomain, rword_schema_skname) REFERENCES rword_schema(skdomain, skname)
+	CONSTRAINT rst_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT rst_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id),
+	CONSTRAINT rst_rs_fkey FOREIGN KEY (rword_schema_skdomain, rword_schema_skname) REFERENCES rword_schema(skdomain, skname)
 );
 
 -- Drop table
@@ -286,9 +286,9 @@ CREATE TABLE stream_binding_tags (
 	stream_binding_stream_name varchar(100) NOT NULL,
 	stream_binding_stream_version int4 NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_qqrfnaftettq8937j5330nhpm UNIQUE (tags_id),
-	CONSTRAINT fkdpowkdybwcgw3octmwdi6bats FOREIGN KEY (stream_binding_infrastructure_name, stream_binding_infrastructure_zone, stream_binding_stream_domain, stream_binding_stream_name, stream_binding_stream_version) REFERENCES stream_binding(infrastructure_name, infrastructure_zone, stream_domain, stream_name, stream_version),
-	CONSTRAINT fkq0lxftkk9w9g6x61ct3s6qisx FOREIGN KEY (tags_id) REFERENCES tag(id)
+	CONSTRAINT sbd_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT sbd_sb_fkey FOREIGN KEY (stream_binding_infrastructure_name, stream_binding_infrastructure_zone, stream_binding_stream_domain, stream_binding_stream_name, stream_binding_stream_version) REFERENCES stream_binding(infrastructure_name, infrastructure_zone, stream_domain, stream_name, stream_version),
+	CONSTRAINT sbd_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id)
 );
 
 -- Drop table
@@ -300,9 +300,9 @@ CREATE TABLE stream_tags (
 	stream_name varchar(100) NOT NULL,
 	stream_version int4 NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_sqfmp0i65j477r5iuaoe7yo93 UNIQUE (tags_id),
-	CONSTRAINT fk7cuiq41n5o8i3f0yteh0gh94e FOREIGN KEY (tags_id) REFERENCES tag(id),
-	CONSTRAINT fko9o8sa5unyttctbanh8w7u6au FOREIGN KEY (stream_domain, stream_name, stream_version) REFERENCES stream(domain, name, version)
+	CONSTRAINT st_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT st_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id),
+	CONSTRAINT st_s_fkey FOREIGN KEY (stream_domain, stream_name, stream_version) REFERENCES stream(domain, name, version)
 );
 
 -- Drop table
@@ -312,7 +312,7 @@ CREATE TABLE stream_tags (
 CREATE TABLE zone_tags (
 	zone_name varchar(100) NOT NULL,
 	tags_id int4 NOT NULL,
-	CONSTRAINT uk_ddty77h480qj0pa7m17h4wbe1 UNIQUE (tags_id),
-	CONSTRAINT fk4b7u4ool5snja8n54ki1sa8qf FOREIGN KEY (tags_id) REFERENCES tag(id),
-	CONSTRAINT fkprjr91h68x1uocir6quvogcaq FOREIGN KEY (zone_name) REFERENCES zone(name)
+	CONSTRAINT zt_ti_ukey UNIQUE (tags_id),
+	CONSTRAINT zt_t_fkey FOREIGN KEY (tags_id) REFERENCES tag(id),
+	CONSTRAINT zt_z_fkey FOREIGN KEY (zone_name) REFERENCES zone(name)
 );
