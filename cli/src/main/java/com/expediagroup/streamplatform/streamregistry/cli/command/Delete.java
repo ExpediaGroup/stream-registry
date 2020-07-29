246a25a69a85979889e13cde9e6fe0e5b2a57c97
Dave Maughan <dmaughan@expediagroup.com>
2020-07-29 15:14:53 +0100

    Add CLI supporting creation and deletion of entities (#257)
    * Add CLI supporting creation and deletion of entities
    
    * Default --topic value to _streamregistry

---
+++ cli/src/main/java/com/expediagroup/streamplatform/streamregistry/cli/command/Delete.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (C) 2018-2020 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.streamplatform.streamregistry.cli.command;
+
+import static com.expediagroup.streamplatform.streamregistry.state.model.event.Event.specificationDeletion;
+import static com.expediagroup.streamplatform.streamregistry.state.model.event.Event.statusDeletion;
+
+import java.util.List;
+
+import lombok.Getter;
+
+import com.expediagroup.streamplatform.streamregistry.cli.action.KafkaEventSenderAction;
+import com.expediagroup.streamplatform.streamregistry.cli.option.EntityOptions;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ConsumerBindingKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ConsumerKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.DomainKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.InfrastructureKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.Key;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ProducerBindingKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ProducerKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.SchemaKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.StreamBindingKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.StreamKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ZoneKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.event.Event;
+import com.expediagroup.streamplatform.streamregistry.state.model.specification.DefaultSpecification;
+import com.expediagroup.streamplatform.streamregistry.state.model.specification.Specification;
+import com.expediagroup.streamplatform.streamregistry.state.model.specification.StreamSpecification;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
+import picocli.CommandLine.Option;
+
+@Command(name = "delete", subcommands = {
+    Delete.Domain.class,
+    Delete.Schema.class,
+    Delete.Stream.class,
+    Delete.Zone.class,
+    Delete.Infrastructure.class,
+    Delete.Producer.class,
+    Delete.Consumer.class,
+    Delete.StreamBinding.class,
+    Delete.ProducerBinding.class,
+    Delete.ConsumerBinding.class
+})
+public class Delete {
+  static abstract class Base<K extends Key<S>, S extends Specification> extends KafkaEventSenderAction {
+    @Option(names = "--statusName") @Getter String statusName;
+
+    @Override
+    public List<Event<?, ?>> events() {
+      if (statusName == null) {
+        return List.of(specificationDeletion(getEntityOptions().key()));
+      } else {
+        return List.of(statusDeletion(getEntityOptions().key(), statusName));
+      }
+    }
+
+    protected abstract EntityOptions<K, S> getEntityOptions();
+  }
+
+  static abstract class Default<K extends Key<DefaultSpecification>> extends Base<K, DefaultSpecification> {}
+
+  @Command(name = "domain")
+  public static class Domain extends Default<DomainKey> {
+    @Mixin @Getter EntityOptions.Domain entityOptions;
+  }
+
+  @Command(name = "schema")
+  static class Schema extends Default<SchemaKey> {
+    @Mixin @Getter EntityOptions.Schema entityOptions;
+  }
+
+  @Command(name = "stream")
+  static class Stream extends Base<StreamKey, StreamSpecification> {
+    @Mixin @Getter EntityOptions.Stream entityOptions;
+  }
+
+  @Command(name = "zone")
+  static class Zone extends Default<ZoneKey> {
+    @Mixin @Getter EntityOptions.Zone entityOptions;
+  }
+
+  @Command(name = "infrastructure")
+  static class Infrastructure extends Default<InfrastructureKey> {
+    @Mixin @Getter EntityOptions.Infrastructure entityOptions;
+  }
+
+  @Command(name = "producer")
+  static class Producer extends Default<ProducerKey> {
+    @Mixin @Getter EntityOptions.Producer entityOptions;
+  }
+
+  @Command(name = "consumer")
+  static class Consumer extends Default<ConsumerKey> {
+    @Mixin @Getter EntityOptions.Consumer entityOptions;
+  }
+
+  @Command(name = "streamBinding")
+  static class StreamBinding extends Default<StreamBindingKey> {
+    @Mixin @Getter EntityOptions.StreamBinding entityOptions;
+  }
+
+  @Command(name = "producerBinding")
+  static class ProducerBinding extends Default<ProducerBindingKey> {
+    @Mixin @Getter EntityOptions.ProducerBinding entityOptions;
+  }
+
+  @Command(name = "consumerBinding")
+  static class ConsumerBinding extends Default<ConsumerBindingKey> {
+    @Mixin @Getter EntityOptions.ConsumerBinding entityOptions;
+  }
+}
---
+++ cli/src/test/java/com/expediagroup/streamplatform/streamregistry/cli/command/ApplyTest.java
@@ -0,0 +1,150 @@
+package com.expediagroup.streamplatform.streamregistry.cli.command;
+
+import static com.google.common.collect.ObjectArrays.concat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.junit.Test;
+
+import com.expediagroup.streamplatform.streamregistry.cli.action.GraphQLEventSenderAction;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ConsumerBindingKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ConsumerKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.DomainKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.InfrastructureKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ProducerBindingKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ProducerKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.SchemaKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.StreamBindingKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.StreamKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.Entity.ZoneKey;
+import com.expediagroup.streamplatform.streamregistry.state.model.event.Event;
+import com.expediagroup.streamplatform.streamregistry.state.model.event.SpecificationEvent;
+import com.expediagroup.streamplatform.streamregistry.state.model.specification.Specification;
+import com.expediagroup.streamplatform.streamregistry.state.model.specification.Tag;
+
+import picocli.CommandLine;
+import picocli.CommandLine.ParseResult;
+
+public class ApplyTest {
+  private final CommandLine underTest = new CommandLine(new Apply());
+
+  private final String[] graphqlOptions = {"--streamRegistryUrl=streamRegistryUrl"};
+  private final String[] configurationOptions = {"--description=description", "--tag=a:b", "--tag=c:d", "--type=type", "--configuration={\"e\":\"f\"}"};
+
+  private final ObjectMapper mapper = new ObjectMapper();
+  private final ObjectNode configuration = mapper.createObjectNode()
+      .put("e", "f");
+
+  private final DomainKey domainKey = new DomainKey("domain");
+  private final SchemaKey schemaKey = new SchemaKey(domainKey, "schema");
+  private final StreamKey streamKey = new StreamKey(domainKey, "stream", 1);
+  private final ZoneKey zoneKey = new ZoneKey("zone");
+  private final InfrastructureKey infrastructureKey = new InfrastructureKey(zoneKey, "infrastructure");
+  private final ProducerKey producerKey = new ProducerKey(streamKey, zoneKey, "producer");
+  private final ConsumerKey consumerKey = new ConsumerKey(streamKey, zoneKey, "consumer");
+  private final StreamBindingKey streamBindingKey = new StreamBindingKey(streamKey, infrastructureKey);
+  private final ProducerBindingKey producerBindingKey = new ProducerBindingKey(producerKey, streamBindingKey);
+  private final ConsumerBindingKey consumerBindingKey = new ConsumerBindingKey(consumerKey, streamBindingKey);
+
+  @Test
+  public void domain() {
+    ParseResult result = parseArgs("domain",
+        "--domain=domain");
+    assertEvent(result, domainKey);
+  }
+
+  @Test
+  public void schema() {
+    ParseResult result = parseArgs("schema",
+        "--domain=domain", "--schema=schema");
+    assertEvent(result, schemaKey);
+  }
+
+  @Test
+  public void stream() {
+    ParseResult result = parseArgs("stream",
+        "--domain=domain", "--stream=stream", "--version=1",
+        "--schemaDomain=schemaDomain", "--schema=schema");
+    assertEvent(result, streamKey);
+  }
+
+  @Test
+  public void zone() {
+    ParseResult result = underTest.parseArgs("zone",
+        "--streamRegistryUrl=streamRegistryUrl",
+        "--description=description", "--tag=a:b", "--tag=c:d", "--type=type", "--configuration={\"e\":\"f\"}",
+        "--zone=zone");
+    assertEvent(result, zoneKey);
+  }
+
+  @Test
+  public void infrastructure() {
+    ParseResult result = parseArgs("infrastructure",
+        "--zone=zone", "--infrastructure=infrastructure");
+    assertEvent(result, infrastructureKey);
+  }
+
+  @Test
+  public void producer() {
+    ParseResult result = parseArgs("producer",
+        "--domain=domain", "--stream=stream", "--version=1", "--zone=zone", "--producer=producer");
+    assertEvent(result, producerKey);
+  }
+
+  @Test
+  public void consumer() {
+    ParseResult result = parseArgs("consumer",
+        "--domain=domain", "--stream=stream", "--version=1", "--zone=zone", "--consumer=consumer");
+    assertEvent(result, consumerKey);
+  }
+
+  @Test
+  public void streamBinding() {
+    ParseResult result = parseArgs("streamBinding",
+        "--domain=domain", "--stream=stream", "--version=1", "--zone=zone", "--infrastructure=infrastructure");
+    assertEvent(result, streamBindingKey);
+  }
+
+  @Test
+  public void producerBinding() {
+    ParseResult result = parseArgs("producerBinding",
+        "--domain=domain", "--stream=stream", "--version=1", "--zone=zone", "--infrastructure=infrastructure", "--producer=producer");
+    assertEvent(result, producerBindingKey);
+  }
+
+  @Test
+  public void consumerBinding() {
+    ParseResult result = parseArgs("consumerBinding",
+        "--domain=domain", "--stream=stream", "--version=1", "--zone=zone", "--infrastructure=infrastructure", "--consumer=consumer");
+    assertEvent(result, consumerBindingKey);
+  }
+
+  private ParseResult parseArgs(String entity, String... args) {
+    return underTest.parseArgs(concat(concat(concat(entity, graphqlOptions), configurationOptions, String.class), args, String.class));
+  }
+
+  private <S extends Specification> void assertEvent(ParseResult result, Entity.Key<S> key) {
+    GraphQLEventSenderAction action = (GraphQLEventSenderAction) result.subcommand().commandSpec().userObject();
+    assertGraphQLOptions(action);
+    List<Event<?, ?>> events = action.events();
+    assertThat(events.size(), is(1));
+    SpecificationEvent<?, ?> event = (SpecificationEvent<?, ?>) events.get(0);
+    assertThat(event.getKey(), is(key));
+    Specification specification = event.getSpecification();
+    assertThat(specification.getDescription(), is("description"));
+    assertThat(specification.getTags(), is(List.of(new Tag("a", "b"), new Tag("c", "d"))));
+    assertThat(specification.getType(), is("type"));
+    assertThat(specification.getConfiguration(), is(configuration));
+
+  }
+
+  private void assertGraphQLOptions(GraphQLEventSenderAction action) {
+    assertThat(action.getStreamRegistryUrl(), is("streamRegistryUrl"));
+  }
+}
