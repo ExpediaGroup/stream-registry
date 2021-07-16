44d814925bcb6272a283d5053b2c92d4ebed456b
Andy Kruth <andy.k@inspiredintellect-us.com>
2021-07-16 16:55:05 -0400

    add Process and ProcessBinding to the model

---
+++ model/src/main/java/com/expediagroup/streamplatform/streamregistry/model/Process.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2018-2021 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.streamplatform.streamregistry.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.expediagroup.streamplatform.streamregistry.model.keys.ProcessKey;
+import com.expediagroup.streamplatform.streamregistry.model.keys.ZoneKey;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Process implements Entity<ProcessKey> {
+  private ProcessKey key;
+  private Specification specification;
+  private List<ZoneKey> zones;
+  private List<ProcessInput> inputs;
+  private List<ProcessOutput> outputs;
+  private Status status;
+}
---
+++ model/src/main/java/com/expediagroup/streamplatform/streamregistry/model/keys/ProcessBindingKey.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2018-2021 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.streamplatform.streamregistry.model.keys;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProcessBindingKey implements Serializable {
+
+  private String domainName;
+  private String zoneName;
+  private String processName;
+
+  public DomainKey getDomainKey() {
+    return new DomainKey(domainName);
+  }
+
+  public ZoneKey getZoneKey() {
+    return new ZoneKey(zoneName);
+  }
+
+}
