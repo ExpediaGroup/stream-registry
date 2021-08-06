bc97bc694d00763a56022216e2f20fb837c62366
jfaulkner <jfaulkner@expediagroup.com>
2021-08-06 13:11:10 +0100

    stupid formatting

--- state/core/src/main/java/com/expediagroup/streamplatform/streamregistry/state/DefaultEntityView.java
+++ state/core/src/main/java/com/expediagroup/streamplatform/streamregistry/state/DefaultEntityView.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2018-2021 Expedia, Inc.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -74,8 +74,8 @@ public class DefaultEntityView implements EntityView {
       .filter(it -> it.getValue().deleted)
       .filter(it -> it.getKey().getClass().equals(keyClass))
       .collect(Collectors.toMap(
-        entry -> (K) entry.getKey(),
-        entry -> Optional.ofNullable((Entity<K, S>) entry.getValue().entity))
+        entry -> (K)entry.getKey(),
+        entry -> Optional.ofNullable((Entity<K, S>)entry.getValue().entity))
       );
   }
 
