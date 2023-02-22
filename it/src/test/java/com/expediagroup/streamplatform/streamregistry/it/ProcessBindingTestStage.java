c64dd880144bd7bc5d8d51689fdd6b636602b3a5
sunlnu <sunlnu@expediagroup.com>
2023-02-22 20:17:55 +0530

    updating grapql query

--- it/src/test/java/com/expediagroup/streamplatform/streamregistry/it/ProcessBindingTestStage.java
+++ it/src/test/java/com/expediagroup/streamplatform/streamregistry/it/ProcessBindingTestStage.java
@@ -125,6 +125,7 @@ public class ProcessBindingTestStage extends AbstractTestStage {
     Response r = client.invoke(factory.upsertProcessBindingMutationBuilder().build());
 
     ProcessBindingsQuery.Data after = (ProcessBindingsQuery.Data) client.getOptionalData(ProcessBindingsQuery.builder().key(query).build()).get();
+
     assertNotNull(after.getProcessBinding().getByQuery().get(0)
         .getFragments().getProcessBindingPart().getStatus().get()
         .getFragments().getStatusPart().getAgentStatus());
