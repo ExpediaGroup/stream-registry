bd4833779fe2eaa0529348099b567c4831e23856
jfaulkner <jfaulkner@expediagroup.com>
2023-09-20 17:10:06 +0100

    fix: STREAMP-9751: Ensure deletes are processed

--- repository/kafka/src/main/java/com/expediagroup/streamplatform/streamregistry/repository/kafka/DefaultRepository.java
+++ repository/kafka/src/main/java/com/expediagroup/streamplatform/streamregistry/repository/kafka/DefaultRepository.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018-2021 Expedia, Inc.
+ * Copyright (C) 2018-2023 Expedia, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -107,6 +107,6 @@ abstract class DefaultRepository<
       send(Event.statusDeletion(stateEntity.getKey(), e.getName()), futures);
     });
     send(Event.specificationDeletion(stateEntity.getKey()), futures);
+    futures.forEach(CompletableFuture::join);
   }
-
 }
